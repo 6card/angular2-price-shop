49307ff7afcffa056857cf2629e496b4b4c1c18f
Barmaley86 <vk_31@mail.ru>
2017-02-20 21:46:23 +0300

    add navigate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import	{Routes,	RouterModule}	from	"@angular/router";
 
 import { AppComponent } from './app.component';
 import { PriceListComponent } from './price-list/price-list.component';
 import { SearchService } from './shared/search.service';
+import { HomeComponent } from './home/home.component';
+
+const	routes:	Routes	=	[
+		{path:	'',	redirectTo:	'home',	pathMatch:	'full'},
+		{path:	'find',	redirectTo:	'search'},
+		{path:	'home',	component:	HomeComponent},
+		{path:	'search',	component:	PriceListComponent},
+		{path:	'**',	component:	HomeComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PriceListComponent
+    PriceListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes,	{useHash:	true})
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]
